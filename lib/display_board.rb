efde5be7985dd50ec21c4169d0fc118ac407b2ea
Dana Palmer <roiai4@hotmail.com>
2018-05-18 01:46:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,3 +5,10 @@ def display_board
   puts -----------
   puts "   |   |   "
 end
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
